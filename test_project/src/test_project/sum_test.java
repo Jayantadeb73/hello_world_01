00f7c64afd6b585c45f51ef32b68e7c58c131dfb
jayanta.deb <debx.jayanta@gmail.com>
2019-06-28 18:46:40 +0530

    sum save....

---
+++ test_project/src/test_project/sum_test.java
@@ -0,0 +1,22 @@
+package test_project;
+
+		import java.util.Scanner; 
+
+		class sum_test
+		{
+		   public static void main(String args[])
+		   {
+		      int x, y, z;
+		 
+		      Scanner in = new Scanner(System.in);
+		     
+		      x = in.nextInt();
+		      y = in.nextInt();
+		      z = x + y;
+		     
+		      System.out.println("Sum=" + z);
+		   }
+		
+	}
+
+
