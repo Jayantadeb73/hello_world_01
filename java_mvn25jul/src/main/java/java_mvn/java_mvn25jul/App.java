6fd9aae13881d39e09badcf776854ab7533b6cf0
Jayanta Deb <jayanta_debx@yahoo.com>
2019-07-25 17:21:03 +0100

    commit on 25jul

---
+++ java_mvn25jul/src/main/java/java_mvn/java_mvn25jul/App.java
@@ -0,0 +1,13 @@
+package java_mvn.java_mvn25jul;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
